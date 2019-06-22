7aebaf9744ca6841f998efe13f92b04aac960d10
Jonas Braun <jbraun@physik.hu-berlin.de>
2019-06-22 19:18:23 +0200

    Working music player

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
+import os
 import time
+import subprocess
+from urllib.parse import urlparse
 
 from smartcard.CardConnectionObserver import ConsoleCardConnectionObserver
 from smartcard.CardMonitoring import CardMonitor, CardObserver
@@ -14,11 +17,34 @@ GET_BLOCK_4 = [0xFF, 0xB0, 0x00, 0x04, 0x10]
 GET_BLOCK_5 = [0xFF, 0xB0, 0x00, 0x05, 0x10]
 GET_BLOCK_6 = [0xFF, 0xB0, 0x00, 0x06, 0x10]
 
+class MusicController:
+
+    def __init__(self):
+        print('music')
+
+        self.p = None
+
+    def play(self, url):
+        print('playing')
+        print(url)
+
+        youtube_code = urlparse(url).path[1:]
+        print(youtube_code)
+        print('playing')
+        print(url)
+
+        self.p = subprocess.Popen(['mplayer', 'brothers.opus'])
+
+    def stop(self):
+        print('stopping')
+
+        self.p.kill()
 
 class PrintObserver(CardObserver):
 
-    def __init__(self):
+    def __init__(self, controller):
         self.observer = ConsoleCardConnectionObserver()
+        self.controller = controller
 
     def update(self, observable, actions):
         (addedcards, removedcards) = actions
@@ -32,29 +58,34 @@ class PrintObserver(CardObserver):
 
             apdu = LOAD_KEY
             response, sw1, sw2 = card.connection.transmit(apdu)
-            print(response)
 
             apdu = AUTHENTICATE
             response, sw1, sw2 = card.connection.transmit(apdu)
-            print(response)
 
+            payload = ''
             apdu = GET_BLOCK_4
             response, sw1, sw2 = card.connection.transmit(apdu)
-            print(response)
+            payload += ''.join([str(chr(_)) for _ in response])
             apdu = GET_BLOCK_5
             response, sw1, sw2 = card.connection.transmit(apdu)
-            print(response)
+            payload += ''.join([str(chr(_)) for _ in response])
             apdu = GET_BLOCK_6
             response, sw1, sw2 = card.connection.transmit(apdu)
-            print(response)
+            payload += ''.join([str(chr(_)) for _ in response])
+            print('payload')
+            print(payload)
+            self.controller.play(payload)
 
         for card in removedcards:
             print('-----', toHexString(card.atr))
+            self.controller.stop()
 
 if __name__ == '__main__':
 
+    controller = MusicController()
+
     card_monitor = CardMonitor()
-    card_observer = PrintObserver()
+    card_observer = PrintObserver(controller)
     card_monitor.addObserver(card_observer)
 
     try:
