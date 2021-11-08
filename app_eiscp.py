ebaf725d4c1fe4fc30686ebe5de5844c1152e0cb
Jonas <jonasclemens@gmail.com>
2021-11-08 19:54:38 +0100

    Add app for Pioneer AV receiver

---
+++ app_eiscp.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import nfc
+import os
+import time
+import subprocess
+import eiscp 
+from urllib.parse import urlparse
+
+
+
+class Reader():
+    def __init__(self):
+        self.music_controller = MusicController()
+
+    def on_connect(self, tag):
+        print(tag)
+        self.music_controller.play(tag.ndef.records[0].uri)
+        return True
+
+    def on_disconnect(self):
+        print('tag removed')
+        self.music_controller.stop()
+
+class MusicController:
+
+    def __init__(self):
+        print('music')
+
+        self.p = None
+
+    def play(self, url):
+        print('playing')
+        print(url)
+
+        youtube_code = urlparse(url).path[1:].split('/')[-1]
+        print(youtube_code)
+        folder = '/var/lib/youtube'
+        for file_ in os.listdir(folder):
+            print(file_)
+            if youtube_code in file_:
+                print('found')
+                filename = file_
+                break
+        else:
+            print('downloading')
+            subprocess.run(['youtube-dl', '-o', folder+'/%(id)s.%(ext)s', '-x', youtube_code])
+            for file_ in os.listdir(folder):
+                if youtube_code in file_:
+                    filename = file_
+                    break
+
+        self.stereo_on()
+
+        print('playing')
+        print(url)
+
+        self.p = subprocess.Popen(['mplayer', os.path.join(folder, filename)],
+                stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+        # TODO add silence track
+
+    def stop(self):
+        print('stopping')
+
+        self.p.kill()
+
+    def stereo_on(self):
+        with eiscp.eISCP('192.168.2.100') as receiver:
+            resp = receiver.raw('PWRQSTN')
+            print(resp)
+
+            receiver.raw('SLI11')  # Input source STRM BOX
+            receiver.command('volume 70')
+
+            if not resp == 'PWR01':
+                time.sleep(7)
+
+
+clf = nfc.ContactlessFrontend('tty:S0')
+
+reader = Reader()
+
+while True:
+    print('listening')
+
+    connection = clf.connect(rdwr={'on-connect': reader.on_connect})
+
+    reader.on_disconnect()
